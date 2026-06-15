cac0b3015ed4cc00fcf99222bdae6127bf208e85
carlosrm12 <carloz88813@gmail.com>
2026-06-14 22:01:18 -0500

    ci(qa): trigger deployment pipeline to test AWS connection and docker compose

--- terraform/environments/qa/main.tf
+++ terraform/environments/qa/main.tf
@@ -1,3 +1,13 @@
+# Configuración del Backend Remoto (HCP Terraform)
+terraform {
+  cloud {
+    organization = "CareUCE"
+    workspaces {
+      name = "careuce-qa"
+    }
+  }
+}
+
 # 1. Llamamos a nuestro módulo de VPC
 module "vpc" {
   source      = "../../modules/vpc"
@@ -54,7 +64,7 @@ resource "aws_instance" "qa_server" {
   iam_instance_profile = "LabInstanceProfile"
   
   # Conexión a la red
-  subnet_id                   = module.vpc.public_subnet_id
+  subnet_id                   = module.vpc.public_subnet_ids[0]
   vpc_security_group_ids      = [aws_security_group.qa_sg.id]
   associate_public_ip_address = true # Para poder acceder desde internet
 
