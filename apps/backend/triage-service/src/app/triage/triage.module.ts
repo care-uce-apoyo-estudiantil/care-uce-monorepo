5f14d58fb2f747e71443b732ee7dcf35ed11f94d
carlosrm12 <carloz88813@gmail.com>
2026-07-09 14:17:55 -0500

    feat(kafka): add kafka

--- apps/backend/triage-service/src/app/triage/triage.module.ts
+++ apps/backend/triage-service/src/app/triage/triage.module.ts
@@ -1,13 +1,32 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { TriageService } from './triage.service';
 import { TriageController } from './triage.controller';
 import { Triage } from './entities/triage.entity';
-import { JwtStrategy } from '../auth/jwt.strategy';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Triage])],
+  imports: [
+    TypeOrmModule.forFeature([Triage]),
+    // Inyectamos el cliente de Kafka
+    ClientsModule.register([
+      {
+        name: 'KAFKA_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: 'triage-service',
+            // En AWS usaremos la URL de MSK, en local tu contenedor de docker
+            brokers: [process.env.KAFKA_BROKER || 'localhost:9092'],
+          },
+          consumer: {
+            groupId: 'triage-consumer-group',
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [TriageController],
-  providers: [TriageService, JwtStrategy],
+  providers: [TriageService],
 })
 export class TriageModule {}
